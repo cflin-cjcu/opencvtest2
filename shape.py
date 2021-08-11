ae275b3b3a2889f1e92c1c68e794208ac7c9bb2d
cflin <cflin@mail.cjcu.edu.tw>
2021-08-11 17:38:50 +0800

    ROI練習二

--- shape.py
+++ shape.py
@@ -1,10 +1,12 @@
 import cv2
 
 img = cv2.imread('./image/test.jpg',cv2.IMREAD_COLOR)
+img2 = cv2.imread('./image/test2.png',cv2.IMREAD_COLOR)
 print(img.shape, img.size)
 
-roi = img[80:180,450:600]
-img[400:500, 300:450] = roi
+# roi = img[80:180,450:600]
+roi = img2[150:490,570:800]
+img[160:500, 100:330] = roi
 
 cv2.imshow('test',img)
 cv2.waitKey(0)
