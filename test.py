0affec4b74ec53de4838ca4c4e615e1d5a8ec461
cflin <cflin@mail.cjcu.edu.tw>
2021-08-11 16:44:07 +0800

    刪除test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-#Import only if not previously imported
-import cv2
-img = cv2.imread("./image/test2.png",cv2.IMREAD_COLOR)     #(flag = 0 or 1 or -1)
-
-#Import only if not previously imported
-import cv2
-cv2.imshow("AAAAAA",img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
