acca36db96072946243599541ad74af4e0bccdb1
cflin <cflin@mail.cjcu.edu.tw>
2021-08-12 11:01:45 +0800

    mouse事件處理

---
+++ mouse.py
@@ -0,0 +1,15 @@
+import cv2
+
+def draw(event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        # cv2.circle(img,(x,y),100,(255,0,0),5)
+        cv2.rectangle(img,(x,y),(x+100,y+100),(0,255,0),5)
+#Import only if not previously imported
+img = cv2.imread("./image/test.jpg")     #(flag = 0 or 1 or -1)
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',draw)
+while(True):
+    cv2.imshow('image',img)
+    if cv2.waitKey(20) == 27:
+        break
+cv2.destroyAllWindows()
