3aa26f1a898312af5e1d12c3e32db2ea0d308206
cflin <cflin@mail.cjcu.edu.tw>
2021-08-12 11:45:22 +0800

    trackbar練習

--- mouse.py
+++ mouse.py
@@ -1,15 +1,19 @@
 import cv2
-
+mode = True
 def draw(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDBLCLK:
-        # cv2.circle(img,(x,y),100,(255,0,0),5)
-        cv2.rectangle(img,(x,y),(x+100,y+100),(0,255,0),5)
+        if mode:
+            cv2.circle(img,(x,y),100,(255,0,0),5)
+        else:
+            cv2.rectangle(img,(x,y),(x+100,y+100),(0,255,0),5)
 #Import only if not previously imported
 img = cv2.imread("./image/test.jpg")     #(flag = 0 or 1 or -1)
 cv2.namedWindow('image')
 cv2.setMouseCallback('image',draw)
 while(True):
     cv2.imshow('image',img)
-    if cv2.waitKey(20) == 27:
+    if cv2.waitKey(20) == ord('m'):
+        mode = not mode
+    elif cv2.waitKey(20) == 27:
         break
 cv2.destroyAllWindows()
--- shape.py
+++ shape.py
@@ -1,9 +1,14 @@
 import cv2
 
+def abc(x):
+    pass
+
 img = cv2.imread('./image/test.jpg',cv2.IMREAD_COLOR)
+temp = img
 img2 = cv2.imread('./image/test2.png',cv2.IMREAD_COLOR)
 print(img.shape, img.size)
-
+cv2.namedWindow('image')
+cv2.createTrackbar('a','image',0,10,abc)
 # img[:,:,1] = 0
 # img[:,:,2] = 0
 
@@ -11,9 +16,16 @@ print(img.shape, img.size)
 roi1 = img[160:500, 100:330]
 roi2 = img2[150:490,570:800]
 
-for i in range(10):
-    roi = cv2.addWeighted(roi1,0.1*i,roi2,1-0.1*i,0)
+while(True):
+    img = temp 
+    time1 = cv2.getTickCount()
+    a = cv2.getTrackbarPos('a','image')
+    roi = cv2.addWeighted(roi1,0.7,roi2,0.3,0)
     img[160:500, 100:330] = roi
-    cv2.imshow('test',img)
-    cv2.waitKey(0) 
-    cv2.destroyAllWindows()
+    cv2.imshow('image',img)
+    time2 = cv2.getTickCount()
+    tt = (time2-time1) / cv2.getTickFrequency()
+    cv2.putText(img,str(tt),(100,100),cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,255),3)
+    if cv2.waitKey(20) == 27:
+        break 
+cv2.destroyAllWindows()
