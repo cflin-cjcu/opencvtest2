c2f114f9d53ddebf4024be0075cf3c023d8e9f8c
cflin <cflin@mail.cjcu.edu.tw>
2021-08-13 11:47:43 +0800

    canny練習3

--- canny2.py
+++ canny2.py
@@ -4,9 +4,10 @@ from matplotlib import pyplot as plt
 def nothing(x):
     pass
 
-img = cv2.imread('./image/test.jpg', 0)
-img = cv2.resize(img,(800,600))
-img = cv2.GaussianBlur(img,(5,5),0)
+img = cv2.imread('./image/test.jpg')
+img = cv2.resize(img,(600,400))
+img1 = cv2.GaussianBlur(img,(5,5),0)
+gray = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
 # sobelx = cv2.Sobel(img, cv2.CV_64F,1,0,ksize=3)
 # sobelx = cv2.convertScaleAbs(sobelx)
 # sobely = cv2.Sobel(img, cv2.CV_64F,0,1,ksize=3)
@@ -19,8 +20,10 @@ cv2.imshow('image',img)
 while(True):
     min = cv2.getTrackbarPos('min','image')
     max = cv2.getTrackbarPos('max','image')
-    edges = cv2.Canny(img, min, max)
+    edges = cv2.Canny(gray, min, max)
+    dst = cv2.bitwise_and(img,img,mask=edges)
     cv2.imshow('canny',edges)
+    cv2.imshow('dst', dst)
     if cv2.waitKey(1) == 27:
         break
 cv2.destroyAllWindows()    
